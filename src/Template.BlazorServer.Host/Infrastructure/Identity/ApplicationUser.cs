541cebae10e2bacf322a996e3efe8bfb38310e0d
usausa <machi.pon@gmail.com>
2026-09-11 17:41:08 +0900

    Update

--- src/Template.BlazorServer.Host/Infrastructure/Identity/ApplicationUser.cs
+++ src/Template.BlazorServer.Host/Infrastructure/Identity/ApplicationUser.cs
@@ -3,6 +3,4 @@ namespace Template.BlazorServer.Host.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 
 // プロフィール項目を追加する場合はここにプロパティを足す
-public sealed class ApplicationUser : IdentityUser
-{
-}
+public sealed class ApplicationUser : IdentityUser;
